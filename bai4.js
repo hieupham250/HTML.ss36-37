318650f9ffc57091d715b1a8d8d12dee9d947d00
hieupham250 <147390672+hieupham250@users.noreply.github.com>
2024-01-24 21:17:22 +0700

    Delete bai4.js

--- bai4.js
+++
@@ -1,7 +0,0 @@
-let a = document.getElementById("a");
-let b = document.getElementById("b");
-let sum = document.getElementById("sum");
-let btn = document.getElementById("btn");
-btn.onclick = function () {
-    sum.value = Number(a.value) + Number(b.value);
-}
